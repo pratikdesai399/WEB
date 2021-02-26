7695434df605aa58e33e16df8e6be36dbb6f516c
pratikdesai399@gmail.com <pratikdesai399@gmail.com>
2021-02-26 18:12:12 +0530

    server.js

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const app = express();
 
 // console.log('HELLO APPLICATION STARTED');
 
-app.get('/',(req,res)=>{
-    res.send("HELLO SERVER STARTED");
-});
+// app.get('/',(req,res)=>{
+//     res.send("HELLO SERVER STARTED");
+// });
+
+app.post('/',(req,res)=>{
+    var email = req.body.email;
+    var amount = req.body.amount;
+
+    //Now to see whether we got the info
+
+    res.send({"amount" : amount , "email" : email});
+})
 
 app.listen(8000,()=>{
     console.log('Server running on port 8000');
