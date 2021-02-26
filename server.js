8e4676b589c92ce9a2070a0fb0ef6f794bb47561
pratikdesai399@gmail.com <pratikdesai399@gmail.com>
2021-02-26 18:26:37 +0530

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser');   // For parsing the request body
 
 // console.log('HELLO APPLICATION STARTED');
 
 // app.get('/',(req,res)=>{
 //     res.send("HELLO SERVER STARTED");
 // });
+
+app.use(bp.json());
 
 app.post('/',(req,res)=>{
     var email = req.body.email;
